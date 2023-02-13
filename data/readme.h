a6e97f3b3e53f61c46e193003abdf16ed61dcdfd
HHuangShao <107794884+HHuangShao@users.noreply.github.com>
2023-02-13 10:41:25 +0800

    Update readme.h

--- data/readme.h
+++ data/readme.h
@@ -1,1 +1,4 @@
 #cas3d
+## Dataset 
+The dataset we used is from CasFlow (https://github.com/Xovee/casflow) , which you can download from this link: 
+[Google Drive](https://drive.google.com/file/d/1o4KAZs19fl4Qa5LUtdnmNy57gHa15AF-/view?usp=sharing) or [Baidu Drive (password: `1msd`)](https://pan.baidu.com/s/1tWcEefxoRHj002F0s9BCTQ).
